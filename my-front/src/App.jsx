fa06c41fcc61e9df8ba3774b7a4327126cca3a64
Lilian Barbosa <liaze.liaz@gmail.com>
2024-03-02 22:30:31 +0000

    react

--- my-front/src/App.jsx
+++ my-front/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 //import reactLogo from "./assets/react.svg";
 //import viteLogo from "/vite.svg";
 import "./App.css";
+import TestHead from "./components/TestHead";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -12,6 +13,7 @@ function App() {
 
   return (
     <>
+      <TestHead></TestHead>
       <div>
         <h2>{header}</h2>
         <p>{info}</p>
---
+++ my-front/src/components/TestHead.jsx
@@ -0,0 +1,3 @@
+export default function TestHead() {
+  return <h1>Test Component one</h1>;
+}
