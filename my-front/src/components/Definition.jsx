d7d6bfc0d8e5ee5d4f80981126a15d9444bcf249
Lilian Barbosa <liaze.liaz@gmail.com>
2024-03-07 11:07:58 +0000

    react

--- my-front/src/components/Definition.jsx
+++ my-front/src/components/Definition.jsx
@@ -4,21 +4,23 @@ import "./Definition.css";
 export default function Definition() {
   return (
     <>
-      <h2>Clinical Depression</h2>
-      <p>
-        Depressive disorder, also known as depression is a common mental
-        disorder. It involves a depressed mood or loss of pleasure or interest
-        in activities for long periods of time. Source: World Health
-        Organization.
-      </p>
+      <div className="Def">
+        <h2>Clinical Depression</h2>
+        <p>
+          Depressive disorder, also known as depression is a common mental
+          disorder. It involves a depressed mood or loss of pleasure or interest
+          in activities for long periods of time. Source: World Health
+          Organization.
+        </p>
 
-      <h2>Anxiety Disorder</h2>
-      <p>
-        Anxiety disorders are characterised by excessive fear and worry and
-        related behavioural disturbances. Symptoms are severe enough to result
-        in significant distress or significant impairment in functioning.
-        Source: World Health Organization.
-      </p>
+        <h2>Anxiety Disorder</h2>
+        <p>
+          Anxiety disorders are characterised by excessive fear and worry and
+          related behavioural disturbances. Symptoms are severe enough to result
+          in significant distress or significant impairment in functioning.
+          Source: World Health Organization.
+        </p>
+      </div>
     </>
   );
 }
